a73823b8aa538c243a361bcf922ea2f95faa2034
mothibimalebana <mothibi.mm12@gmail.com>
2025-08-25 18:05:22 +0200

    chore: fileController now communicates with the services effiecently

--- src/controllers/fileController.js
+++ src/controllers/fileController.js
@@ -10,11 +10,34 @@ exports.getAllFiles = async (req, res) => {
   }
 };
 
-exports.createUserFile = async (req, res) => {
+exports.createFile = async (req, res) => {
   try {
-    const user = await User.createUserFile(req.body);
-    res.status(201).json(user);
+    console.log(req.body)
+    const file = await User.createUserFile(req.body);
+    res.status(201).json(file);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
+  exports.updateFile = async (req, res) => {
+    try {
+      console.log(req.body)
+      const file = await User.updateUserFile(Number(req.params.id) , req.body)
+      res.status(201).json(file)
+    }
+    catch (err) {
+      res.status(500).json({ error: err.message })
+    }
+  };
+
+  exports.deleteFile = async (req, res) => {
+    try {
+      console.log(req.body)
+      const user = await User.deleteUserFile(Number(req.params.id))
+      res.status(201).json(user)
+    }
+    catch (err) {
+      res.status(500).json({ error: err.message })
+    }
+  };
--- src/routes/fileRoute.js
+++ src/routes/fileRoute.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const router = express.Router();
+const fileRouter = express.Router();
 const { getAllFiles, createUserFile } = require('../controllers/userController');
 
-router.get('/', getAllFiles);
-router.post('/form', createUserFile);
+fileRouter.get('/', getAllFiles);
+fileRouter.post('/form', createUserFile);
 
 module.exports = router;
