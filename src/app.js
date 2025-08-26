c0ef123fd3cb16785532769f0d98a4bf8b7672fe
mothibimalebana <mothibi.mm12@gmail.com>
2025-08-26 17:43:28 +0200

    chore: reconfigured routes to

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const userRoutes = require('./routes/userRoute');
+const fileRouter = require('./routes/fileRoute');
 
 
 
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api/user', userRoutes);
+app.use('/api/:userId/files',fileRouter )
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
--- src/routes/fileRoute.js
+++ src/routes/fileRoute.js
@@ -2,10 +2,10 @@ const express = require('express');
 const fileRouter = express.Router();
 const { getFile, getAllFiles, createFile, updateFile, deleteFile } = require('../controllers/fileController');
 
-fileRouter.get('/files', getAllFiles);
-fileRouter.get('/files/:fileId', getFile)
-fileRouter.post('file/form', createFile);
-fileRouter.put('/file/:id/form', updateFile)
-fileRouter.delete('files/:fileId', deleteFile)
+fileRouter.get('/', getAllFiles);
+fileRouter.get('/:fileId', getFile)
+fileRouter.post('form', createFile);
+fileRouter.put('/:fileId/form', updateFile)
+fileRouter.delete('/:fileId', deleteFile)
 
 module.exports = fileRouter;
