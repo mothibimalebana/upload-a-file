31cd5736b07b1a0fc4f05be77bb9074eaeb86871
mothibimalebana <mothibi.mm12@gmail.com>
2025-09-20 19:16:37 +0200

    chore: set up session based authentication using passportjs and prisma store library

--- src/app.js
+++ src/app.js
@@ -1,19 +1,35 @@
 require('dotenv').config();
 const express = require('express');
-const userRoutes = require('./routes/userRoute');
-const session = require("express-session");
 const passport = require('passport');
-const { getUser } = require('./controllers/userController');
 const { User } = require('./services/userServices');
 const authRouter = require('./routes/auth');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-
+const expressSession = require('express-session');
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+const { PrismaClient } = require('@prisma/client');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(
+  expressSession({
+    cookie: {
+     maxAge: 7 * 24 * 60 * 60 * 1000 // ms
+    },
+    secret: 'a santa at nasa',
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+      new PrismaClient(),
+      {
+        checkPeriod: 2 * 60 * 1000,  //ms
+        dbRecordIdIsSessionId: true,
+        dbRecordIdFunction: undefined,
+      }
+    )
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.json());
