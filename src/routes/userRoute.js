85ddc687ae6790e28c068663985f9a108c391504
mothibimalebana <mothibi.mm12@gmail.com>
2025-09-23 18:58:46 +0200

    user createUser controller is working

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,11 +2,11 @@ const express = require('express');
 const userRouter = express.Router();
 const { createUser, fetchUser, fetchAllUsers, updateUser, deleteUser } = require('../controllers/userController');
 
-userRouter.get('/', fetchAllUsers)
-userRouter.get('/:id', fetchAllUsers)
+userRouter.get('/', fetchAllUsers);
+userRouter.get('/:id', fetchUser);
 userRouter.post('/', createUser);
-userRouter.put('/:id', updateUser)
-userRouter.delete('/:id', deleteUser)
+userRouter.put('/:id', updateUser);
+userRouter.delete('/:id', deleteUser);
 
 module.exports = userRouter;
  
